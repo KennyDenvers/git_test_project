1a2368c88052dd84eaeaa77b6c6397f2e3a592d4
KennyDenvers <kaidom71gmail.com>
2025-04-18 19:00:22 +0200

    Add basic Streamlit app

---
+++ app.py
@@ -0,0 +1,8 @@
+# app.py
+
+import streamlit as st
+
+st.title("Hello from Streamlit!")
+st.write("You've just turned your GitHub repo into a web app. 🎉")
+
+st.success("Let’s build your ESG dashboard next!")
